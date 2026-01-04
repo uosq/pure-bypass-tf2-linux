4baff248df2ed62a99c58a732c7c76be0c8a761f
uosq <lboxwallytf2@gmail.com>
2026-01-04 17:29:58 -0300

    Make sig better

--- src/main.c
+++ src/main.c
@@ -15,7 +15,7 @@ void Hooked_CL_CheckForPureServerWhitelist(void** pFilesToReload)
 __attribute__((constructor))
 void init()
 {
-	originalCheckForPureServerWhitelistFn = (CL_CheckForPureServerWhitelistFn)sigscan_module("engine.so", "83 3D FD 8A 65 00 01 7E 77 80 3D A8 13 66 00 00 75 6E 48 8D 05 AF 96 6B");
+	originalCheckForPureServerWhitelistFn = (CL_CheckForPureServerWhitelistFn)sigscan_module("engine.so", "83 3D ? ? ? ? 01 7E ? 80 3D ? ? ? ? ? 75 ?");
 	detour_init(&CL_CheckForPureServerWhitelist_ctx, (void*)originalCheckForPureServerWhitelistFn, (void*)&Hooked_CL_CheckForPureServerWhitelist);
 	detour_enable(&CL_CheckForPureServerWhitelist_ctx);
 }
